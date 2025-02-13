1d6da5ca97b7d491bdf101da3f6da6f4e591bb1b
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-13 16:56:48 +0100

    add complete dice exercise

--- dadi/main.js
+++ dadi/main.js
@@ -6,3 +6,28 @@ javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
 si ma noi cosa vogliamo fare?
 torniamo a scrivere in italiano
 proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
+
+let userRoll = prompt("Roll the dice! Type 'Roll' to try yout luck!", "Roll")
+
+let userDice;
+
+if (userRoll === "Roll") {
+    userDice = Math.floor(Math.random() * 6 + 1)
+} else {
+    alert("Come on, at least try!")
+}
+
+let botDice = Math.floor(Math.random() * 6 + 1);
+
+console.log("Your roll: " + userDice);
+console.log("Our roll: " + botDice);
+
+if (userDice == undefined) {
+    alert("You gotta type 'Roll'");
+} else if (userDice > botDice) {
+    alert("YOU WON! Nothing... but you still won!!")
+} else if (userDice < botDice) {
+    alert("YOU LOSE! WOMP WOMP")
+} else if (userDice = botDice) {
+    alert("IT'S A TIE!")
+}
