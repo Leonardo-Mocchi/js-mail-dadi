dcd856ff35181134c5e4b552f2129598bd5fba2e
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-13 16:34:10 +0100

    mail done

--- mail/main.js
+++ mail/main.js
@@ -6,3 +6,36 @@ eh si!  Basta ragionare un po’.
 :puntare_a_destra: Bonus:
 Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
 Nota: Non è necessario provvedere alla validazione delle email */
+
+const verifiedMails = ["mahatma.ghandi@gmail.com", "leonardo.davinci@hotmail.it", "albert.einstein@genius.com", "galileo@galilei.it", "isaac.newton@sir.us"]
+
+let userInput = prompt("Digit your mail here", "example@google.com")
+
+let access;
+
+console.log(verifiedMails);
+console.log(userInput);
+console.log();
+
+for (let i = 0; i < verifiedMails.length; i++) {
+    const verifiedPpl = verifiedMails[i];
+    console.log(verifiedPpl);
+
+    if (verifiedPpl === userInput) {
+        access = true;
+    } else {
+        access = false;
+    }
+}
+console.log();
+console.log(access);
+
+if (access === true) {
+    alert("SUCCESS!! You've been granted access")
+    console.log("Welcome " + userInput);
+} else if (access === false) {
+    alert("DENIED ACCESS Your email is not in our Guest List")
+    console.log("Sorry " + userInput + " your email is not in our Guest List");
+} else {
+    alert("There's been an error, please try again")
+}
